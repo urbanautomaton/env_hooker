394329a5e61d900657cc577ae1d3bf3c94eebd28
Simon Coffey <simon@urbanautomaton.com>
2016-12-28 19:43:24 +0000

    Shellcheck pacification

--- test/run_env_hook_test.sh
+++ test/run_env_hook_test.sh
@@ -1,7 +1,6 @@
 # shellcheck shell=bash
 # vi: set ft=sh:
 
-# shellcheck source=/dev/null
 . ./test/helper.sh
 
 function enter_dummy_project() {
@@ -18,7 +17,7 @@ function exit_dummy_project() {
 function setUp() {
   enter_count=0
   exit_count=0
-  cd "$test_root"
+  cd "$test_root" || exit
   mkdir -p "$test_project_dir"
   touch "${test_project_dir}/.dummyproject"
 }
@@ -29,10 +28,7 @@ function tearDown() {
 }
 
 function test_calls_enter_function_with_directory() {
-  cd "$test_project_dir"
-  # Okay so the problem is trap DEBUG doesn't run until the end of the
-  # function (look up "simple command" maybe?)
-  # http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_12_02.html
+  cd "$test_project_dir" || exit
   __env_hooker_run_env_hook .dummyproject dummy_project
 
   assertEquals \
@@ -42,49 +38,50 @@ function test_calls_enter_function_with_directory() {
 }
 
 function test_calls_enter_function_only_once_if_still_inside_project() {
-  cd "$test_project_dir"
+  cd "$test_project_dir" || exit
   __env_hooker_run_env_hook .dummyproject dummy_project
   __env_hooker_run_env_hook .dummyproject dummy_project
 
   assertEquals \
     "is called exactly once" \
-    "0" \
+    1 \
     "$enter_count"
 }
 
 function test_does_not_call_exit_function_if_inside_project() {
-  cd "$test_project_dir"
+  cd "$test_project_dir" || exit
   __env_hooker_run_env_hook .dummyproject dummy_project
 
   assertEquals \
     "is not called" \
-    "0" \
+    0 \
     "$exit_count"
 }
 
 function test_calls_exit_function_if_exiting_project() {
-  cd "$test_project_dir"
+  cd "$test_project_dir" || exit
   __env_hooker_run_env_hook .dummyproject dummy_project
-  cd "$test_root"
+  cd "$test_root" || exit
   __env_hooker_run_env_hook .dummyproject dummy_project
 
   assertEquals \
     "is called" \
-    "1" \
+    1 \
     "$exit_count"
 }
 
 function test_calls_exit_function_only_once_if_exiting_project() {
-  cd "$test_project_dir"
+  cd "$test_project_dir" || exit
   __env_hooker_run_env_hook .dummyproject dummy_project
-  cd "$test_root"
+  cd "$test_root" || exit
   __env_hooker_run_env_hook .dummyproject dummy_project
   __env_hooker_run_env_hook .dummyproject dummy_project
 
   assertEquals \
-    "is called" \
-    "1" \
+    "is called exactly once" \
+    1 \
     "$exit_count"
 }
 
+# shellcheck source=/dev/null
 SHUNIT_PARENT=$0 . $SHUNIT2
